df130f63aa6705317ed1a58944094fee8b7cbf0a
@tri.tnp <tri.tnp@vgamestd.com>
2025-05-19 19:23:23 +0700

    admin

--- services/api-service/src/modules/admin/admin.controller.ts
+++ services/api-service/src/modules/admin/admin.controller.ts
@@ -2,7 +2,7 @@ import { ApiOkResponseCustom, CheckAbilites, CurrentUser } from "@common/decorat
 import { ResponseType } from "@common/dtos";
 import { AdminActionEnum, SubjectEnum } from "@common/enums";
 import { JwtAccessTokenAuthGuard } from "@modules/auth/guards";
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { ApiBearerAuth, ApiBody, ApiTags } from "@nestjs/swagger";
 import { AdminLoginRequestDto, AdminLoginResponseDto, AdminResponseDto, CreateAdminDto, TokenPayloadAdminDto } from "./dtos";
 import { AdminAbilitiesGuard, LocalAuthGuard } from "./guards";
@@ -20,6 +20,14 @@ export class AdminController {
     @ApiOkResponseCustom(ResponseType, AdminResponseDto)
     async signUpAdminAccount(@Body() user: CreateAdminDto): Promise<ResponseType> {
         return this.adminService.signUpAdminAccount(user);
+    }
+
+    @CheckAbilites({ action: AdminActionEnum.READ, subject: SubjectEnum.ADMINS })
+    @UseGuards(JwtAccessTokenAuthGuard, AdminAbilitiesGuard)
+    @Get("getAdminInfo")
+    @ApiOkResponseCustom(ResponseType, AdminResponseDto)
+    async getAdminInfo(@CurrentUser() adminInfo: TokenPayloadAdminDto): Promise<ResponseType> {
+        return this.adminService.findOneAdminById(adminInfo._id);
     }
 
     @Post("login")
